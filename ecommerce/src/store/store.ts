137dcb7f48d872b95977958761876f43e196594c
startender <mathsitedu@mail.ru>
2022-09-07 01:31:43 +0300

    redux persist init

--- ecommerce/src/store/store.ts
+++ ecommerce/src/store/store.ts
@@ -1,6 +1,18 @@
 import { createStore } from 'redux';
-import RootReducer from './rootReducer';
+import rootReducer from './rootReducer';
 
-const store = createStore(RootReducer)
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"
 
-export default store
+const persistConfig = {
+  key: "root",
+  storage,
+  whiteList: ['card'],
+};
+
+const persistedReducer = persistReducer<any, any>(persistConfig, rootReducer);
+
+let store = createStore(persistedReducer);
+let persistor = persistStore(store);
+
+export { store, persistor }
